f3a4a46057021b460391e7354cf29feaeb2d54b8
MrHamdan <mr.hamdanahmed@gmail.com>
2021-10-29 17:57:32 +0600

    fix dynamic link

--- src/Pages/BookService/BookService.js
+++ src/Pages/BookService/BookService.js
@@ -9,41 +9,35 @@ import './BookService.css'
 const BookService = () => {
     const { serviceId } = useParams();
     const { user } = useAuth();
-    const [singleServices, setSingleServices] = useState([]);
 
-    const [singleService, setSingleService] = useState({});
+    const [service, setService] = useState({})
+
+    useEffect(() => {
+        fetch(`http://localhost:5000/services/${serviceId}`)
+            .then(res => res.json())
+            .then(data => setService(data))
+    }, [])
+
 
     const { register, handleSubmit, watch, formState: { errors } } = useForm();
     const onSubmit = data => console.log(data);
 
-    useEffect(() => {
-        fetch("http://localhost:5000/services")
-            .then((res) => res.json())
-            .then((data) => setSingleServices(data));
-    }, []);
-
-    useEffect(() => {
-        const singleDetail = singleServices.find(
-            (employee) => employee._id === serviceId
-        );
-        setSingleService(singleDetail);
-    }, [singleServices]);
 
     return (
         <div>
             <div>
-                <h1 className="text-design pt-5 pb-5">Here's Full Detail's Of Our {singleService?.title} Service.</h1>
+                <h1 className="text-design pt-5 pb-5">Here's Full Detail's Of Our {service?.title} Service.</h1>
                 <div className="border-4 mb-3 mx-2">
                     <div className="row g-0">
                         <div className="col-md-6">
-                            <img src={singleService?.img} className="img-fluid rounded-start" alt="..." />
+                            <img src={service?.img} className="img-fluid rounded-start" alt="..." />
                         </div>
                         <div className="col-md-6">
                             <div className="card-body">
-                                <h5 className="card-title">{singleService?.title}</h5>
-                                <p className="card-text">{singleService?.description}</p>
-                                <p className="card-text"><small className="text-muted">{singleService?.location}</small></p>
-                                <span>Price: $ {singleService?.price}</span>
+                                <h5 className="card-title">{service?.title}</h5>
+                                <p className="card-text">{service?.description}</p>
+                                <p className="card-text"><small className="text-muted">{service?.location}</small></p>
+                                <span>Price: $ {service?.price}</span>
                                 <div>
                                     <form className="rounded-3xl container mx-2" onSubmit={handleSubmit(onSubmit)}>
                                         <h1>Booking Info.</h1>
